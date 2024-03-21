9a446b33741432bedd62bf9404f2595865a89cff
Razia Pervez <160026294+RaziaPervez@users.noreply.github.com>
2024-03-21 11:02:17 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,23 @@
+// used of if, else if, and else statements
+//GRADE CHECKER
+var studentScore = 75;
+// If the score is 90 or above, return "A"
+if (studentScore >= 90 && studentScore <= 99) {
+    console.log("student got A grade");
+    // If the score is between 80 and 89 (inclusive), return "B"
+}
+else if (studentScore >= 80 && studentScore <= 89) {
+    console.log("student got B grade");
+    // If the score is between 70 and 79 (inclusive), return "C"
+}
+else if (studentScore >= 70 && studentScore <= 79) {
+    console.log("student got C grade");
+    // If the score is between 60 and 69 (inclusive), return "D"
+}
+else if (studentScore >= 60 && studentScore <= 69) {
+    console.log("student got D grade");
+    // If the score is below 60, return "F"
+}
+else {
+    console.log("student got F grade");
+}
